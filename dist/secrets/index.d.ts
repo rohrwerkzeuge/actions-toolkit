1931cbac97c993b74a2c5826d23f48f0b17f0bf3
Mark Anderson <emer@emer.net>
2025-04-16 18:57:42 -0400

    Fix name

--- dist/secrets/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class CloudSecret {
-    token: string;
-    constructor(token: string);
-    getField(name: string, field: string): Promise<string>;
-    getCredential(name: string): Promise<string>;
-}

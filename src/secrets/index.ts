8328d4f68255bf768684e47031ee2e43b3fd6e1f
Mark Anderson <emer@emer.net>
2025-04-16 18:47:22 -0400

    Inital Package Commit

---
+++ src/secrets/index.ts
@@ -0,0 +1,23 @@
+import { createClient } from "@1password/sdk";
+
+export class CloudSecret {
+    token: string;
+
+    constructor(token: string) {
+        this.token = token
+    }
+
+    async getField(name: string, field: string): Promise<string> {
+        const client = await createClient({
+            auth: this.token,
+            integrationName: "Update Upstream Integration",
+            integrationVersion: "0.1.0",
+        });
+
+        return client.secrets.resolve(`op://cloud/${name}/${field}`);
+    }
+
+    async getCredential(name: string): Promise<string> {
+        return this.getField(name, "credential")
+    }
+}
